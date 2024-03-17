58902f1601e7649fd65709fb24e557463e7ad475
ValeriyGolyshev <valeriy1golyshev@gmail.com>
2024-03-17 14:33:27 +0700

    Web built with canvaskit renderer

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -27,9 +27,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "9b8d83197bcf0be3bba05e57bfcce909",
-"/": "9b8d83197bcf0be3bba05e57bfcce909",
-"main.dart.js": "bd1981adc6e496868865c9c6cec62a99",
+"index.html": "389457fa78dc24f6b26d3c427b5ee876",
+"/": "389457fa78dc24f6b26d3c427b5ee876",
+"main.dart.js": "0e84d7c0786a1336023723f61ca3b19d",
 "manifest.json": "970cfbbb275dd03d6bc6ddc7bf513657",
 "version.json": "7f60df4bf71e08f07be40ddd39d8ad21"};
 // The application shell files that are downloaded before a service worker can
